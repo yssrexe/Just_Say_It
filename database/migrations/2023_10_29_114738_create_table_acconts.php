e06adf54e16624e24ec5824ad89b9fa4e69a58bc
yssrexe <elyasiniyassir40@gmail.com>
2023-10-29 12:48:44 +0100

    todolist

--- database/migrations/2023_10_29_114738_create_table_acconts.php
+++ database/migrations/2023_10_29_114738_create_table_acconts.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('families', function (Blueprint $table) {
+        Schema::create('table_acconts', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('email');
             $table->string('password');
             $table->timestamps();
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('families');
+        Schema::dropIfExists('table_acconts');
     }
 };
