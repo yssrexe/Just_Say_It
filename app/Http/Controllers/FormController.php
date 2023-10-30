3a4202219ba0befe7d2dd5f7d131206953292aea
yssrexe <elyasiniyassir40@gmail.com>
2023-10-30 13:33:14 +0100

    todo list

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Accounts;
+use Illuminate\Support\Facades\Hash;
 
 class FormController extends Controller
 {
@@ -17,6 +18,7 @@ class FormController extends Controller
             'password'=> 'required|string',
         ]);
 
+        $validatedData['password'] = Hash::make($validatedData['password']);
         // Create a new record in the database
         Accounts::create($validatedData);
 
