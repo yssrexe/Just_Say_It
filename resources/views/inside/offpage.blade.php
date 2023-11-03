2eab4df74b228eba145409fda6d7786f06b22d1d
yssrexe <elyasiniyassir40@gmail.com>
2023-11-03 22:40:15 +0100

    todo list

--- resources/views/inside/offpage.blade.php
+++ resources/views/inside/offpage.blade.php
@@ -34,12 +34,36 @@
         </form>
     </div>
     <div>
-        <p><span id="datetime"></span></p>
+
     </div>
 
   <div id="myUnOrdList">
-        <ul class="todo-list">
-        </ul>
+    <div class="card">
+        <div class="comments">
+            <div class="comment-react">
+            </div>
+            <div class="comment-container">
+                <div class="user">
+                <div class="user-pic">
+                    <svg fill="none" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linejoin="round" fill="#707277" stroke-linecap="round" stroke-width="2" stroke="#707277" d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z"></path>
+                  <path stroke-width="2" fill="#707277" stroke="#707277" d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z"></path>
+                    </svg>
+                </div>
+                <div class="user-info">
+                    <span>anonyme</span>
+                    <p><span id="datetime"></span></p>
+                </div>
+                </div>
+                <p class="comment-content">
+                    <ul class="todo-list">
+                    </ul>
+
+                </p>
+            </div>
+        </div>
+    </div>
+
     </div>
 
     <script>
@@ -251,6 +275,43 @@
 
     </script>
     <style>
+        .card {
+  width: 450px;
+  height: fit-content;
+  background-color: white;
+  box-shadow: 0px 187px 75px rgba(0, 0, 0, 0.01), 0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09), 0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 17px 17px 27px 27px;
+}
+
+.comments {
+  display: grid;
+  grid-template-columns: 35px 1fr;
+  gap: 20px;
+  padding: 20px;
+}
+
+.user-info span{
+    margin-top:10px;
+    font-size: 20px;
+}
+
+.comment-container .user {
+  display: flex;
+  grid-template-columns: 40px 1fr;
+  gap: 10px;
+}
+
+.comment-container .user .user-pic {
+  width: 40px;
+  height: 40px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f1f1f1;
+  border-radius: 50%;
+}
+
         * {
             margin: 0;
             padding: 0;
